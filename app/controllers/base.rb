20feae978f91254bfc4580972ea70c19fa5e6845
igoru <igorsantos07@gmail.com>
2012-02-24 04:13:43 -0200

    ! fixing crazy thing about url param

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -1,7 +1,8 @@
 Memelinks.controllers :base do
 
   get :image, :map => '/:slug', :priority => :low do
-    meme = Meme.find_by_slug params[:slug][0]
+    slug = if params[:slug].is_a? Array then params[:slug][0] else params[:slug] end
+    meme = Meme.find_by_slug slug
     if meme
       content_type meme.image_mime
       body meme.image
