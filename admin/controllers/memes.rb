d65ec4dbf80e4f68fc5c8e45304f8f9e80713b28
Igoru <igorsantos07@gmail.com>
2012-02-25 10:48:03 -0200

    + adding a URL field to the meme form; now it's possible to create a meme using directly an image URL

--- admin/controllers/memes.rb
+++ admin/controllers/memes.rb
@@ -13,9 +13,19 @@ Admin.controllers :memes do
   post :create do
     @meme = Meme.new
     @meme.name = params[:meme]['name']
-    @meme.image_mime = params[:meme]['image'][:head].match(/Content-Type: ([\w\/-_]*)/i)[1]
-    @meme.image = params[:meme]['image'][:tempfile].read
     @meme.creator = current_account
+
+    if !params[:meme]['image_file'].nil?
+      @meme.image_mime  = params[:meme]['image_file'][:head].match(/Content-Type: ([\w\/-_]*)/i)[1]
+      @meme.image       = params[:meme]['image_file'][:tempfile].read
+    elsif !params[:meme]['image_url'].empty?
+      require 'net/http'
+      image             = Net::HTTP.get_response URI(params[:meme]['image_url'])
+      if image.is_a? Net::HTTPSuccess
+        @meme.image_mime  = image.get_fields('content-type')[0]
+        @meme.image       = image.body
+      end
+    end
 
     if @meme.save
       flash[:notice] = 'Meme was successfully created.'
