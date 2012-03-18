aa8e0048223733bc0390cda26a4d60d4b3a344e7
Igoru <igorsantos07@gmail.com>
2012-03-18 18:16:13 -0300

    + adding "suggest a meme". closes #3

--- app/controllers/meme.rb
+++ app/controllers/meme.rb
@@ -2,22 +2,23 @@
 Memelinks.controllers :meme do
 
   get :index, :map => '/' do
-    @memes = Meme.limit(10).tops
+    @memes = Meme.limit(10).tops.active
     render 'meme/index'
   end
 
   get :all, :map => '/todos' do
-    @memes = Meme.tops
+    @memes = Meme.tops.active
     render 'meme/index'
   end
 
   get :image, :map => '/:filename', :priority => :low do
+    from_admin = !request.referrer.nil? and request.referrer.include? request.host + '/admin/'
     filename = if params[:filename].is_a? Array then params[:filename][0] else params[:filename] end
-    meme = Meme.find_by_filename filename
+    meme = (from_admin)? Meme.find_by_filename(filename) : Meme.active.find_by_filename(filename)
 
     if meme
       if !params['y'].nil? or request.referer.nil? or (
-        !request.referrer.include? request.host + '/admin/' and #if it doesn't come from admin pages
+        !from_admin and #if it doesn't come from admin pages
         !(request.referrer =~ Regexp.new(request.host+"\/?")) and #or from the homepage
         !request.referrer.include? request.host + url(:meme,:all) #or from :all action
       )
@@ -38,7 +39,7 @@ Memelinks.controllers :meme do
 
   get :search, :map => '/search' do
     regexp = Regexp.new params[:meme].downcase
-    @memes = Meme.any_of({:name_lower => regexp}, {:keywords => regexp})
+    @memes = Meme.active.any_of({:name_lower => regexp}, {:keywords => regexp})
 
     @message = case @memes.length
       when 0 then '<img src="/images/memes/rage-face.gif" /> Não foi encontrado nenhum meme com essas palavras...'
@@ -47,6 +48,35 @@ Memelinks.controllers :meme do
     end
 
     render 'meme/index'
+  end
+
+  get :suggest, :map => '/sugira' do
+    render 'meme/suggest'
+  end
+
+  post :suggest, :map => '/sugira' do
+    @meme = Meme.new
+    @meme.name    = params['meme']['name']
+    @meme.status  = :pending
+    @meme.ip_user_creator = request.ip
+
+    if !params['meme']['image_url'].nil?
+      @meme.image_url = params['meme']['image_url']
+    elsif !params['meme']['image_file'].nil?
+      @meme.image_file = params['meme']['image_file']
+    end
+
+    if !@meme.valid?
+      errors = ''
+      @meme.errors.each {|field,msg| errors += "#{field}: #{msg}; " }
+      logger.error errors
+
+      render 'meme/suggest'
+    else
+      @meme.save
+      flash[:notice] = 'Muito obrigado! Sua sugestão foi salva e, se aprovada, entrará para o acervo do site :D'
+      redirect '/'
+    end
   end
 
 end
