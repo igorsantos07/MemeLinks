e865205a4fbe36e133248ba0fa64cc6e0c9ef1af
Igoru <igorsantos07@gmail.com>
2012-04-20 01:17:59 -0300

    + re-fixing problem of Sass plugin in both apps. refs #46

--- lib/sass_initializer.rb
+++ lib/sass_initializer.rb
@@ -5,15 +5,9 @@ module SassInitializer
     # Enables support for SASS template reloading in rack applications.
     # See http://nex-3.com/posts/88-sass-supports-rack for more details.
     # Store SASS files (by default) within 'app/stylesheets'
-
-    folders = case app.to_s.downcase
-      when 'memelinks'  then {:template => 'app/stylesheets', :css => 'public/stylesheets'}
-      when 'admin'      then {:template => 'admin/stylesheets', :css => 'public/admin/stylesheets'}
-    end
-
     require 'sass/plugin/rack'
-    Sass::Plugin.options[:template_location] = Padrino.root folders[:template]
-    Sass::Plugin.options[:css_location] = Padrino.root folders[:css]
+    Sass::Plugin.options[:template_location] = Padrino.root '/stylesheets'
+    Sass::Plugin.options[:css_location] = Padrino.root '/public/stylesheets'
     Sass::Plugin.options[:style] = :compressed
     app.use Sass::Plugin::Rack
   end
